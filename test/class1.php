e6c3ef831862d401f1c06f83c4c4a23c8afa7208
Roman Olyvko <r.olyvko@zinitsolutions.com>
2016-12-18 22:58:15 +0200

    c1

--- test/class1.php
+++ test/class1.php
@@ -3,7 +3,7 @@ class class1 extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('welcome_message');//t1
+        $this->load->view('welcome_message');//t1 t11
     }
 
 }
